c40d9341ac404b7ddbda7ca2d681e673b8b2e62a
Uildson S. dos Santos <wilder_shon@hotmail.com>
2021-10-08 23:04:59 -0300

    Created a new workspace for the initial backend

---
+++ src/main/java/com/devsuperior/exec/tdd/cap03/validation_and_security/ValidationAndSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.exec.tdd.cap03.validation_and_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidationAndSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValidationAndSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/exec/tdd/cap03/validation_and_security/ValidationAndSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.exec.tdd.cap03.validation_and_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidationAndSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
