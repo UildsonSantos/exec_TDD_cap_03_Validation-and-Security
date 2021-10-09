ef7306d0c8657d7c051c65f9a786c7c3855b8545
Uildson S. dos Santos <wilder_shon@hotmail.com>
2021-10-09 00:44:12 -0300

    Test implementation for 'insert in city controller' feature when any client is connected and wants to register a city and the city has the valid data

--- src/test/java/com/devsuperior/exec/tdd/cap03/validation_and_security/controllers/CityControllerIntegrationTests.java
+++ src/test/java/com/devsuperior/exec/tdd/cap03/validation_and_security/controllers/CityControllerIntegrationTests.java
@@ -3,6 +3,7 @@ package com.devsuperior.exec.tdd.cap03.validation_and_security.controllers;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -12,6 +13,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.exec.tdd.cap03.validation_and_security.dto.CityDTO;
+import com.devsuperior.exec.tdd.cap03.validation_and_security.tests.TokenUtil;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootTest
@@ -25,6 +27,18 @@ public class CityControllerIntegrationTests {
 	@Autowired
 	private ObjectMapper objectMapper;
 	
+	@Autowired
+	private TokenUtil tokenUtil;
+	
+	private String clientUsername;
+	private String clientPassword;
+	
+	@BeforeEach
+	void setUp() throws Exception {		
+		clientUsername = "ana@gmail.com";
+		clientPassword = "123456";
+	}
+	
 	@Test
 	public void insertShouldReturnUnauthorizedWhenNoUserLogged() throws Exception {
 		CityDTO cityDTO = new CityDTO(null, "Recife");
@@ -35,5 +49,19 @@ public class CityControllerIntegrationTests {
 				.contentType(MediaType.APPLICATION_JSON)
 				.accept(MediaType.APPLICATION_JSON))
 		.andExpect(status().isUnauthorized());		
+	}
+	
+	@Test
+	public void insertShouldReturnForbiddenWhenClientLogged() throws Exception {		
+		String accessToken = tokenUtil.obtainAccessToken(mockMvc, clientUsername, clientPassword);		
+		CityDTO cityDTO = new CityDTO(null, "Recife");
+		String jsonBody = objectMapper.writeValueAsString(cityDTO);
+		
+		mockMvc.perform(post("/cities")
+				.header("Authorization", "Bearer " + accessToken)
+				.content(jsonBody)
+				.contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON))
+		.andExpect(status().isForbidden());
 	}
 }
